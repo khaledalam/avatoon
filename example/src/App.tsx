1618aaf355a4d16012ad8c4b95ac1acdfb405fa2
Khaled Alam <khaledalam.net@gmail.com>
2025-05-08 21:09:34 +0400

    feat: disable arm and hands animation while talking, fix example.

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
 import { Avatoon } from 'avatoon';
-
-const visemeJson = {
-  visemes: [
-    { time: 0, viseme: 'A' },
-    { time: 0.2, viseme: 'B' },
-    { time: 0.4, viseme: 'D' },
-  ],
-  audio_base64: '', // add a real one for full effect
-};
+import './App.css';
+import visemeJson from './visemeData.json';
 
 function App() {
   return (
-    <Avatoon
-      // glbUrl="https://raw.githubusercontent.com/khaledalam/avatoon/main/test/assets/placeholder-avatar.glb"
-      glbUrl={'../public/avatar.glb'}
-      goal={'Normal'}
-      onRenderComplete={() => console.log('Render Completed!')}
-      visemeJson={visemeJson}
-    />
+    <div className="container">
+      <Avatoon
+        // glbUrl="https://raw.githubusercontent.com/khaledalam/avatoon/main/test/assets/placeholder-avatar.glb"
+        glbUrl={'/avatar.glb'}
+        goal={'Normal'}
+        onRenderComplete={() => console.log('Render Completed!')}
+        visemeJson={visemeJson}
+        showPlayVoiceButton={true}
+      />
+    </div>
   );
 }
 
